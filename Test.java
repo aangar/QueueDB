65e361597c0f1ac47024312cb9d06f1652e14457
Alvaro Angarita <alvaro.angarita@interclypse.com>
2022-08-01 11:13:15 -0400

    getters and setters

--- Test.java
+++ Test.java
@@ -1,24 +1,15 @@
 public class Test {
     public static void main(String[] args) {
-        int totalRows = 4;
-        int whiteSpaceMax = totalRows + 1;
-        int[][] items = new int[whiteSpaceMax + totalRows][whiteSpaceMax + totalRows];
-        int numeric = 1;
+        int[] values = {1,2,3,4,5,6,7,8,9,10};
+        TreeNode tree = generateTree(values);
+        System.out.println(tree.getLeftNode().getRightValue());
+    }
 
-        for (int row = 0; row < totalRows; row++) {
-            for (int index = 1; index < whiteSpaceMax; index += 2 ) {
-                items[row][index] = numeric;
-                numeric++;
-            }
-        }
-
-        for (int row = 0; row < totalRows; row++) {
-            for (int index = 0; index < whiteSpaceMax; index++ ) {
-                System.out.print(String.format("[%d] ", items[row][index]));
-            }
-            System.out.println(" ");
-        }
-
+    private static TreeNode generateTree(int[] values) {
+        TreeNode root = new TreeNode(values[0]);
+        root.setLeft(values[1]);
+        root.setRight(values[2]);
+        return root;
     }
 }
 
@@ -27,7 +18,40 @@ class TreeNode {
     private TreeNode left;
     private TreeNode right;
 
+    public TreeNode() { }
+
     public TreeNode(int val) {
         this.val = val;
+    }
+
+    public void setLeft() { }
+    public void setRight() { }
+
+    public void setLeft(int a) {
+        this.left = new TreeNode(a);
+    }
+
+    public void setRight(int a) {
+        this.right = new TreeNode(a);
+    }
+
+    public int getLeftValue() {
+        return this.left.getValue();
+    }
+
+    public int getRightValue() {
+        return this.right.getValue();
+    }
+
+    public TreeNode getRightNode() {
+        return this.right;
+    }
+
+    public TreeNode getLeftNode() {
+        return this.left;
+    }
+
+    public int getValue() {
+        return this.val;
     }
 }
