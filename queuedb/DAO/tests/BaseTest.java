410ded4eda15369125054e04b2b0995a21b5fa39
Alvaro Angarita <alvaro.angarita@interclypse.com>
2022-08-11 10:41:27 -0400

    QueueDB - BaseTest Introduced

---
+++ queuedb/DAO/tests/BaseTest.java
@@ -0,0 +1,63 @@
+package queuedb.DAO.tests;
+
+import java.io.File;
+
+/**
+ * The BaseTest class
+ */
+public class BaseTest {
+    /**Color for a successful test */
+    public final String SUCCESS_COLOR = "\u001B[32m";
+    /**Color for a failing test */
+    public final String FAIL_COLOR = "\u001B[31m";
+    /**reset color??? */
+    public final String ANSI_RESET = "\u001B[0m";
+    /**The directory to the testing collection. */
+    public String TEST_COLLECTION_DIR;
+    /**The test File name. just for output reasons :D */
+    public String TestFileName;
+
+    /**
+     * Logs the result to the console for test status.
+     * @param result the result of the test. Set by the user, must output to a boolean.
+     * @param testName the name of the test.
+     */
+    public void logTestResult(boolean result, String testName) {
+        String value = result ? "PASSED" : "FAILED";
+        String color = result ? this.SUCCESS_COLOR : this.FAIL_COLOR;
+        String f = "%s:%s %s%s%s";
+        String formatter = String.format(f, this.TestFileName, testName, color, value, ANSI_RESET);
+        System.out.println(formatter);
+
+    }
+
+    /**
+     * Clears the test collection. Does not delete.
+     */
+    public void clearTestCollection() {
+        if (this.TEST_COLLECTION_DIR == null || this.TEST_COLLECTION_DIR.isEmpty()) {
+            System.err.println("Collection Directory not specified, unable to clear.");
+            return;
+        }
+        File baseDir = new File(TEST_COLLECTION_DIR);
+        String[] existingFiles = baseDir.list();
+        if (existingFiles.length < 1) {
+            return;
+        }
+        if (existingFiles.length >= 1) {
+            for (String file : existingFiles) {
+                if (file.equals("test")) {
+                    continue;
+                }
+                new File(TEST_COLLECTION_DIR + file).delete();
+            }
+        }
+    }
+
+    /**
+     * Removes the test collection after testing.
+     */
+    public void removeTestCollection() {
+        new File(this.TEST_COLLECTION_DIR).delete();
+    }
+}
--- queuedb/DAO/tests/SampleDocumentDAOTest.java
+++ queuedb/DAO/tests/SampleDocumentDAOTest.java
@@ -4,26 +4,21 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.stream.Collectors;
-
-import queuedb.DatabaseParser;
 import queuedb.DAO.SampleDocumentDAO;
 import queuedb.Objects.SampleDocument;
 
-public class SampleDocumentDAOTest {
-    private final SampleDocumentDAO sampDocDAO;
-    private static final String SUCCESS_COLOR = "\u001B[32m";
-    private static final String FAIL_COLOR = "\u001B[31m";
-    private static final String ANSI_RESET = "\u001B[0m";
-    private String TEST_COLLECTION_DIR;
+public class SampleDocumentDAOTest extends BaseTest{
+    public final SampleDocumentDAO sampDocDAO;
+
 
     public SampleDocumentDAOTest(String dir) {
         this.sampDocDAO = new SampleDocumentDAO(dir);
         this.TEST_COLLECTION_DIR = dir;
+        this.TestFileName = "SampleDocumentDAOTest";
     }
 
     public void runTests() {
@@ -31,7 +26,7 @@ public class SampleDocumentDAOTest {
         test_save();
         test_saveOne();
         test_findAll();
-        new File(this.TEST_COLLECTION_DIR).delete();
+        this.removeTestCollection();
     }
 
     /**
@@ -45,10 +40,7 @@ public class SampleDocumentDAOTest {
                 .collect(Collectors.toList());
         List<SampleDocument> savedDocuments = this.sampDocDAO.saveDocuments(toSaveDocuments);
         List<SampleDocument> foundDocs = this.sampDocDAO.findAll();
-        boolean result = savedDocuments.size() == foundDocs.size();
-        String value = result ? "PASSED" : "FAILED";
-        String color = result ? SUCCESS_COLOR : FAIL_COLOR;
-        System.out.println("SampleDocumentDAOTest:test_save: " + color + value + ANSI_RESET);
+        this.logTestResult(savedDocuments.size() == foundDocs.size(), "test_save");
         this.clearTestCollection();
     }
 
@@ -57,12 +49,7 @@ public class SampleDocumentDAOTest {
         boolean saved = this.sampDocDAO.saveOne(document);
 
         List<SampleDocument> foundDocs = this.sampDocDAO.findAll();
-
-        boolean result = saved && (foundDocs.size() == 1);
-        String value = result ? "PASSED" : "FAILED";
-        String color = result ? SUCCESS_COLOR : FAIL_COLOR;
-        System.out.println("SampleDocumentDAOTest:test_saveOne: " + color + value +
-                ANSI_RESET);
+        this.logTestResult(saved && (foundDocs.size() == 1), "test_SaveOne");
         this.clearTestCollection();
     }
 
@@ -99,30 +86,7 @@ public class SampleDocumentDAOTest {
         List<SampleDocument> foundDocs = this.sampDocDAO.findAll();
 
         boolean result = (foundDocs.size() == filesInTest.length) && (filesInTest.length == toSaveDocuments.size());
-        String value = result ? "PASSED" : "FAILED";
-        String color = result ? SUCCESS_COLOR : FAIL_COLOR;
-        System.out.println("SampleDocumentDAOTest:test_findAll: " + color + value + ANSI_RESET);
+        this.logTestResult(result, "test_findAll");
         this.clearTestCollection();
     }
-
-    private void clearTestCollection() {
-        if (this.TEST_COLLECTION_DIR == null || this.TEST_COLLECTION_DIR.isEmpty()) {
-            System.err.println("Collection Directory not specified, unable to clear.");
-            return;
-        }
-        File baseDir = new File(TEST_COLLECTION_DIR);
-        String[] existingFiles = baseDir.list();
-        if (existingFiles.length < 1) {
-            return;
-        }
-        if (existingFiles.length >= 1) {
-            for (String file : existingFiles) {
-                if (file.equals("test")) {
-                    continue;
-                }
-                new File(TEST_COLLECTION_DIR + file).delete();
-            }
-        }
-    }
-
 }
